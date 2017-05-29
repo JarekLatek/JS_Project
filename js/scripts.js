fc9fa6481edec049681300492baacb2d502ff3d3
Jarek Latek <jareklatek@gmail.com>
2017-05-29 14:14:18 +0200

    Add triangle field to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,10 @@ alert('test');
 // console.log(variable);
 
 var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
+	a = 3;
+	h = 4;
+	triangleArea = a*h/2;
+
+alert('Hello, ' + name + '. Trinagle field with base a: ' + a + ' and hight h: ' + h + ' is equal to: ' + triangleArea);
+
+console.log('Trinagle field with base a: ' + a + ' and hight h: ' + h + ' is equal to: ' + triangleArea);
